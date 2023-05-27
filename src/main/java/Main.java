fb198996d9f48328a7e42791ae104f80cdf53e9e
mohammad sajjad <m.s.e.r1382@gmail.com>
2023-05-27 16:57:17 +0330

    Start CLI creation
    start creating command line user interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,7 @@
-import db.DBConnection;
-import db.models.Category;
-
-import java.util.ArrayList;
-import java.util.Scanner;
+import cli.CommandLineInterface;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        DBConnection.connect();
-        System.out.println("please enter name");
-        String name = sc.next();
-        System.out.println("please enter inst");
-        String inst = sc.next();
-        System.out.println("please enter category");
-        String cat = sc.next();
-        ArrayList<String> cats = new ArrayList<>();
-        cats.add(cat);
-        System.out.println("please enter ingr");
-        String ing = sc.next();
-        ArrayList<String> ings = new ArrayList<>();
-        ings.add(ing);
-        DBConnection.addRecipe(name, inst, cats, ings);
-        DBConnection.close();
+        CommandLineInterface.start();
     }
 }
---
+++ src/main/java/cli/CommandLineInterface.java
@@ -0,0 +1,7 @@
+package cli;
+
+public abstract class CommandLineInterface {
+    public static void start() {
+        System.out.println("command line interface is started.");
+    }
+}
